58ac60c853c485c73df15db5a765dbb53c953337
Brandon Mills <sllimnodnarb@gmail.com>
2017-05-10 17:03:37 -0500

    adding files associated with user-story-3-mentoravail

--- code/Kele/lib/kele.rb
+++ code/Kele/lib/kele.rb
@@ -1,18 +1,35 @@
 require 'httparty'
+require 'json'
 
 class Kele
 
   include HTTParty
 
   def initialize(email, password)
-    @bloc_base_api_url = "https://www.bloc.io/api/v1"
-    @auth_token = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { email: email, password: password })
+    @auth_token = response["auth_token"]
     raise "Wrong email and/or password" if @auth_token.nil?
   end
 
   def get_me
-    @current_user = self.class.get('/users/me', headers: { "authorization" => @auth_token})
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
     @parsed_response = JSON.parse(response.body)
+    puts @parsed_response["mentor_id"]["student_availability"]
+  end
+
+  def get_mentor_availability(mentor_id)
+    @mentor_id = mentor_id
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{@mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @mentor_timeslots = JSON.parse(response.body)
+    timeslots = []
+    @mentor_timeslots.each do |timeslot|
+      if timeslot["booked"] == nil
+          timeslots << timeslot
+        else
+          puts 'your mentor aint got time for anyone else bro, hes full up'
+      end
+    end
+    puts timeslots
   end
 
 end
